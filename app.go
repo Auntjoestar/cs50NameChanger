c1284f3a19848cc76dd39365bb477ea4977dc873
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2024-10-05 16:27:58 -0600

    Add rename feature

--- app.go
+++ app.go
@@ -3,6 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+	// sqlite ORM
+	// "gorm.io/gorm"
+	// "gorm.io/driver/sqlite"
 )
 
 // App struct
@@ -23,5 +31,67 @@ func (a *App) startup(ctx context.Context) {
 
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+	names := [2]string{"Mario", "Luis"}
+	return fmt.Sprintf("Hello %s, It's show time!", names[1])
+}
+
+func (a *App) ListPrograms() []string {
+	programs := []string{"cs50", "web50", "CodeKids"}
+	return programs
+}
+
+func (a *App) ListCycles() []string {
+	cycles := []string{"Y24C1", "Y24C2", "Y24C3"}
+	return cycles
+}
+
+func (a *App) ListWeeks() []string {
+	weeks := []string{"S01", "S02", "S03"}
+	return weeks
+}
+
+func (a *App) ListGroups() []string {
+	groups := []string{"A", "B", "C"}
+	return groups
+}
+
+func (a *App) MakeNewName(program string, cycle string, week string, group string) string {
+	if program == "" || cycle == "" || week == "" || group == "" {
+		return "Nombre del archivo"
+	}
+	return fmt.Sprintf("%s_%s_%s_grupo%s_foto", program, cycle, week, group)
+}
+
+func (a *App) OpenFileDialog() ([]string, error) {
+	filepath, err := runtime.OpenMultipleFilesDialog(a.ctx, runtime.OpenDialogOptions{
+		Title: "Ingresa los archivos que desee modificar",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return filepath, nil
+}
+
+func (a *App) ChangeFileNames(files []string, newName string) error {
+	for i := 0; i < len(files); i++ {
+		file := files[i]
+		dir := filepath.Dir(file)
+		fileName := filepath.Base(file)
+		newFile := filepath.Join(dir, newName+strconv.Itoa(i+1)+filepath.Ext(fileName))
+		err := os.Rename(file, newFile)
+		if err != nil {
+			return err
+		}
+	}
+	/*
+		for _, file := range files {
+			dir, filename := filepath.Split(file)
+			newFile := filepath.Join(dir, newName+filepath.Ext(filename))
+			err := os.Rename(file, newFile)
+			if err != nil {
+				return err
+			}
+		}
+	*/
+	return nil
 }
--- go.mod
+++ go.mod
@@ -33,7 +33,7 @@ require (
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
 
 // replace github.com/wailsapp/wails/v2 v2.9.2 => C:\Users\sebal\go\pkg\mod
